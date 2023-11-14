c9b1f248c838cd910f038f5da8b10f51c37fae3b
Anto Idicherian Lonappan <antolonappan@icloud.com>
2023-11-14 11:27:00 -0800

    initial commit
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

---
+++ lbxs4/lb_sims/simulations.py
@@ -0,0 +1,15 @@
+from .foreground import Foregrounds,CompSep
+from .noise import NoiseModel
+from .cmb import CMBlensed
+
+
+class LBmaps:
+    
+    def __init__(self,nside):
+        pass
+    
+    def TQU(freq,idx):
+        pass
+    
+    def CompSep_TQU(idx):
+        pass
